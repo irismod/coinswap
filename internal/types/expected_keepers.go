678606fdc8d89ee822767b7e883ccc560c99563e
zhangyelong <yelong@bianjie.ai>
2020-04-08 09:54:27 +0800

    Fix liquidity supply

--- internal/types/expected_keepers.go
+++ internal/types/expected_keepers.go
@@ -3,10 +3,12 @@ package types
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authexported "github.com/cosmos/cosmos-sdk/x/auth/exported"
+	supplyexported "github.com/cosmos/cosmos-sdk/x/supply/exported"
 )
 
 // SupplyKeeper defines the expected supply keeper
 type SupplyKeeper interface {
+	GetSupply(ctx sdk.Context) supplyexported.SupplyI
 	GetModuleAddress(name string) sdk.AccAddress
 	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
@@ -25,4 +27,3 @@ type BankKeeper interface {
 type AccountKeeper interface {
 	GetAccount(ctx sdk.Context, addr sdk.AccAddress) authexported.Account
 }
-
