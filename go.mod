a04b9fdbc5dfa268076891225fadbe086ea81650
Segue <huoda.china@163.com>
2020-04-25 14:36:20 +0800

    bump sdk version

--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module github.com/irismod/coinswap
 go 1.13
 
 require (
-	github.com/cosmos/cosmos-sdk v0.34.4-0.20200410083150-b7397d6df548
+	github.com/cosmos/cosmos-sdk v0.34.4-0.20200423152229-f1fdde5d1b18
 	github.com/gorilla/mux v1.7.4
-	github.com/spf13/cobra v0.0.7
-	github.com/tendermint/tendermint v0.33.3
+	github.com/spf13/cobra v1.0.0
+	github.com/tendermint/tendermint v0.33.4
 )
 
 replace github.com/keybase/go-keychain => github.com/99designs/go-keychain v0.0.0-20191008050251-8e49817e8af4
--- internal/types/expected_keepers.go
+++ internal/types/expected_keepers.go
@@ -3,27 +3,24 @@ package types
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authexported "github.com/cosmos/cosmos-sdk/x/auth/exported"
-	supplyexported "github.com/cosmos/cosmos-sdk/x/supply/exported"
+	bankexported "github.com/cosmos/cosmos-sdk/x/bank/exported"
 )
-
-// SupplyKeeper defines the expected supply keeper
-type SupplyKeeper interface {
-	GetSupply(ctx sdk.Context) supplyexported.SupplyI
-	GetModuleAddress(name string) sdk.AccAddress
-	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
-	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
-	BurnCoins(ctx sdk.Context, name string, amt sdk.Coins) error
-	MintCoins(ctx sdk.Context, name string, amt sdk.Coins) error
-}
 
 // BankKeeper defines the expected bank send keeper
 type BankKeeper interface {
 	SendCoins(ctx sdk.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
 	GetAllBalances(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
 	GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin
+
+	GetSupply(ctx sdk.Context) bankexported.SupplyI
+	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
+	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+	BurnCoins(ctx sdk.Context, name string, amt sdk.Coins) error
+	MintCoins(ctx sdk.Context, name string, amt sdk.Coins) error
 }
 
 // AccountKeeper defines the expected account keeper
 type AccountKeeper interface {
 	GetAccount(ctx sdk.Context, addr sdk.AccAddress) authexported.Account
+	GetModuleAddress(name string) sdk.AccAddress
 }
